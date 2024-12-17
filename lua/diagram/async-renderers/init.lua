a0ac8732773a5db808bb4db4aea75016b46a4f0b
Marat Gumerov <marat.gumerof@aspose.com>
2024-12-17 19:02:35 +0800

    fix: require fix

--- lua/diagram/async-renderers/init.lua
+++ lua/diagram/async-renderers/init.lua
@@ -1,10 +1,10 @@
-local debounce = require('diagram/renderers/debounce-renderer')
-local mermaid = require("diagram/renderers/mermaid")
+local debounce = require('diagram/async-renderers/debounce-renderer')
+local mermaid = require("diagram/async-renderers/mermaid")
 -- local plantuml = require("diagram/renderers/plantuml")
 -- local d2 = require("diagram/renderers/d2")
 
 return {
-  mermaid = debounce.setup(mermaid, 1000),
-  --  plantuml = plantuml,
-  -- d2 = d2,
+    mermaid = debounce.setup(mermaid, 1000),
+    --  plantuml = plantuml,
+    -- d2 = d2,
 }
